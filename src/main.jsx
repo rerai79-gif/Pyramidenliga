5e66aa388131fca657564e99cd186c58f4e185cd
rerai79-gif <re.rai79@googlemail.com>
2026-05-11 19:39:47 +0200

    Clean Vite deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import PyramidLeagueApp from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import PyramidLeagueApp from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PyramidLeagueApp />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
